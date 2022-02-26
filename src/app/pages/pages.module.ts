1fa381e723dbb09110e5fcef110d8bc1e21a3abb
Muhammad Adip Kolili <adipkolili@gmail.com>
2022-02-27 04:08:35 +0700

    wip - token multisender

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -5,19 +5,22 @@ import { FooterComponent } from '../components/footer/footer.component';
 import { MainpageModuleRoutes } from './pages-routing.module';
 import { RouterModule } from '@angular/router';
 import { MainComponent } from './main/main.component';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { TokenMultisenderComponent } from '../views/token-multisender/token-multisender.component';
 
 
 @NgModule({
   declarations: [
     HeaderComponent,
     FooterComponent,
-    MainComponent
+    MainComponent,
+    TokenMultisenderComponent
   ],
   imports: [
+    RouterModule.forChild(MainpageModuleRoutes),
     CommonModule,
     FormsModule,
-    RouterModule.forChild(MainpageModuleRoutes)
+    ReactiveFormsModule
   ]
 })
 export class PagesModule { }
--- src/app/views/token-multisender/token-multisender.component.ts
+++ src/app/views/token-multisender/token-multisender.component.ts
@@ -6,10 +6,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./token-multisender.component.scss']
 })
 export class TokenMultisenderComponent implements OnInit {
-
+  step = 1;
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  navigateStep(step: number) {
+    if(step <= 3) {
+      this.step = step;
+    }
   }
 
 }
