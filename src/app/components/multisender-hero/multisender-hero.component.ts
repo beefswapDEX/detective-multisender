3f4d367cc0febb209e0d3c4fa01d5711e199ef33
Sandydht <sandydwi75@gmail.com>
2022-02-27 11:58:48 +0700

    feat: create multisender hero component

---
+++ src/app/components/multisender-hero/multisender-hero.component.ts
@@ -0,0 +1,51 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-multisender-hero',
+  templateUrl: './multisender-hero.component.html',
+  styleUrls: ['./multisender-hero.component.scss']
+})
+
+export class MultisenderHeroComponent implements OnInit {
+  networks = [
+    {
+      name: "ETH Mainnet",
+      imagePath: "assets/images/networks/eth.svg"
+    },
+    {
+      name: "BSC Mainnet",
+      imagePath: "assets/images/networks/bsc.svg"
+    },
+    {
+      name: "Polygon Mainnet",
+      imagePath: "assets/images/networks/polygon.svg"
+    },
+    {
+      name: "Fantom Mainnet",
+      imagePath: "assets/images/networks/fantom.svg"
+    },
+    {
+      name: "Solana Mainnet",
+      imagePath: "assets/images/networks/solana.svg"
+    },
+    {
+      name: "Avalance Mainnet",
+      imagePath: "assets/images/networks/avalance.svg"
+    },
+    {
+      name: "Polkadot Mainnet",
+      imagePath: "assets/images/networks/polkadot.svg"
+    },
+    {
+      name: "Okex Mainnet",
+      imagePath: "assets/images/networks/okex.svg"
+    },
+    {
+      name: "Huobi Mainnet",
+      imagePath: "assets/images/networks/huobi.svg"
+    },
+  ]
+
+  constructor() { }
+  ngOnInit(): void { }
+}
