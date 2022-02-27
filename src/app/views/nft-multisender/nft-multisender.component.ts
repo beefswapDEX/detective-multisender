a6b45b984f1eed1e7609820e318b99fde21f5e68
Sandydht <sandydwi75@gmail.com>
2022-02-28 03:38:40 +0700

    feat: add function step indicator

--- src/app/views/nft-multisender/nft-multisender.component.ts
+++ src/app/views/nft-multisender/nft-multisender.component.ts
@@ -6,10 +6,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./nft-multisender.component.scss']
 })
 export class NftMultisenderComponent implements OnInit {
+  step = 1;
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  navigateStep(step: number) {
+    if(step <= 3) {
+      this.step = step;
+    }
+  }
 }
