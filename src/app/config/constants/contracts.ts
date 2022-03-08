eacf2b0cc7e327650ec50f95553458c11e4a6bba
Muhammad Adip Kolili <adipkolili@gmail.com>
2022-03-08 23:33:59 +0700

    rollback

--- src/app/config/constants/contracts.ts
+++ src/app/config/constants/contracts.ts
@@ -3,12 +3,12 @@ export default {
     97: '0x1469069401C03F67abd9f83d6CEe7Ed2cab7f25F',
     56: '0x1469069401C03F67abd9f83d6CEe7Ed2cab7f25F',
   },
-  upgradeableProxySender: {
-    97: '0xF15b345FB9296294347b5056462ACb5D9F00ED5F',
-    56: '0xF15b345FB9296294347b5056462ACb5D9F00ED5F',
-  },
-  eternalStorageProxyForDetectiveMultisender: {
+  EternalStorageProxyForDetectiveMultisender: {
     97: '0xB0764Da8FA1801dcaa57512A45Da6E213775D3DA',
     56: '0xB0764Da8FA1801dcaa57512A45Da6E213775D3DA'
-  }
+  },
+  // eternalProxyForStormMultisender: {
+  //   97: '0xa5025faba6e70b84f74e9b1113e5f7f4e7f4859f',
+  //   56: '0xa5025faba6e70b84f74e9b1113e5f7f4e7f4859f'
+  // }
 }
